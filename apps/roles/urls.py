82a3915b4b64bd762ecbc11403aa86b1011ab734
sergio <smgalu@gmail.com>
2015-04-08 13:00:55 -0400

    roles urls

---
+++ apps/roles/urls.py
@@ -0,0 +1,9 @@
+__author__ = 'sergio'
+
+from django.conf.urls import patterns, url #include
+
+urlpatterns = patterns('',
+                       url(r'^$', 'django.contrib.auth.views.login',
+                           {'template_name': 'inicio/index.html'}, name='login'),
+                       url(r'^cerrar/$', 'django.contrib.auth.views.logout_then_login', name='logout')
+)
--- apps/roles/views.py
+++ apps/roles/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Rol
 
 # Create your views here.
+
+class RolesView(ListView): # ListView importa todos los objetos de un modelo
+    template_name = 'listar_roles.html'
+    model = Rol
