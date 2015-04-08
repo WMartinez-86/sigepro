ec7edc87ce1f9eb4336b0a71025c9a95cb98e528
sergio <smgalu@gmail.com>
2015-04-08 16:16:30 -0400

    roles todavia no me abre la pagina roles =/

---
+++ apps/roles/forms.py
@@ -0,0 +1,12 @@
+__author__ = 'smgalu'
+
+from django import forms
+from apps.roles.models import Rol
+from django.contrib.auth.models import Group, Permission
+
+
+class GroupForm(forms.ModelForm):
+    #permissions = Group.objects.exclude(permission_id=1,permission_id=2,permission_id=3)
+    class Meta:
+        model = Group
+        #fields=['name','permissions']
--- apps/roles/urls.py
+++ apps/roles/urls.py
@@ -1,9 +1,22 @@
 __author__ = 'sergio'
 
-from django.conf.urls import patterns, url #include
+from django.conf.urls import patterns, url, include
+from django.contrib import admin
+admin.autodiscover()
+
+from .views import crear_rol, lista_roles, detalle_rol, editar_rol, RegisterSuccessView
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^$', 'django.contrib.auth.views.login',
-                           {'template_name': 'inicio/index.html'}, name='login'),
-                       url(r'^cerrar/$', 'django.contrib.auth.views.logout_then_login', name='logout')
-)
+    url(r'^crear/$', crear_rol, name='crear_rol'),
+    url(r'^$',lista_roles,name='lista_roles'),
+    url(r'^(?P<id_rol>\d+)$', detalle_rol, name='detalle_rol'),
+#    url(r'^eliminar/(?P<id_rol>\d+)$', eliminar_rol, name='eliminar_rol'),
+    url(r'^modificar/(?P<id_rol>\d+)$', editar_rol, name='editar_rol'),
+#    url(r'^search/$',buscarRol, name='buscar_roles'),
+    url(r'^register/success/$',RegisterSuccessView.as_view()),
+    )
+
+
+# url(r'^listar_roles.html/', RolesView.as_view()),
