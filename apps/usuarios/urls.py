742ee354475a5593a032d3e738d396594716be12
willian <willianmartinez86@gmail.com>
2015-04-10 19:32:04 -0400

    urls para app usuarios

---
+++ apps/usuarios/urls.py
@@ -0,0 +1,13 @@
+__author__ = 'willian'
+from django.conf.urls import patterns, include, url
+from .views import list_usuario, edit_user, delete_user, search
+urlpatterns = patterns('',
+    url(r'^$', list_usuario, name='usuario'),
+    url(r'^edit/(?P<pk>\d+)/$', edit_user, name='usuario_edit'),
+    url(r'^delete/(?P<pk>\d+)/$', delete_user, name='usuario_delete'),
+    url(r'^nuevo_pass/$','apps.usuarios.views.password_change',name='cambiar_pass_done'),
+    url(r'^nuevo_pass/done/$', 'apps.usuarios.views.password_change_done', name='cambiar_pass_done'),
+    url(r'^search/$', search, name='search'),
+
+
+)
