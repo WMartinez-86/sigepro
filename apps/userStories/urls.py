2523eeeae8c784adb91a321ac20901de45869a8b
juanma yegros <juanmayegros@gmail.com>
2015-05-01 23:44:21 -0400

    userStories urls

--- apps/userStories/urls.py
+++ apps/userStories/urls.py
@@ -2,14 +2,14 @@ __author__ = 'juanma'
 
 from django.conf.urls import patterns, url
 from django.contrib import admin
-from apps.userStories.views import listar_userStories
+from apps.userStories.views import listar_userStories, crear_userStory
 
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
 
-                       #url(r'^userStory/crear/(?P<id_tipoItem>\d+)$',crear_userStory, name='crear_userStory'),
+                       url(r'^crear/$',crear_userStory, name='crear_userStories'),
                        url(r'^$',listar_userStories, name='listar_userStories'),
                        #url(r'^userStory/detalle/(?P<id_userStory>\d+)$',detalle_userStory, name='detalle_userStory'),
                        #url(r'^userStory/modificar/(?P<id_userStory>\d+)$',editar_userStory, name='editar_userStory'),
