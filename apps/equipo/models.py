a00877f1bfa692de929e4f2f35047034b6ebe184
juanma yegros <juanmayegros@gmail.com>
2015-05-08 00:40:59 -0400

    equipo modelo de equipo de trabajo

---
+++ apps/equipo/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User, Group
+from apps.proyectos.models import Proyecto
+
+# Create your models here.
+
+
+
+class MiembroEquipo(models.Model):
+    """
+    Miembros del equipo de un proyecto con un rol específico
+    @cvar usuario: Clave foranea a un Usuario
+    @cvar proyecto: Clave foranea a un Proyecto
+    @cvar roles: Clave muchos a muchos a Roles
+    """
+    usuario = models.ForeignKey(User)
+    proyecto = models.ForeignKey(Proyecto)
+    roles = models.ManyToManyField(Group)
