98541bf275129c59927822a2bf6592344c22f5d6
sergio <smgalu@gmail.com>
2015-05-23 01:32:26 -0400

    lista equipo

--- apps/equipos/views.py
+++ apps/equipos/views.py
@@ -22,35 +22,18 @@ def ver_equipo(request, id_proyecto):
     @return: render_to_response('proyectos/ver_equipo.html', {'proyectos':dato,'lider': lider, 'comite':comite, 'usuarios':usuarios}, context_instance=RequestContext(request))
     """
     rolSM = Group.objects.filter(name = "Scrum Master")
-    SM = MiembroEquipo.objects.get(rol = rolSM, proyecto_id = id_proyecto)
-    SMUser = User.objects.get(id = SM.usuario_id)
-
+    haySM = MiembroEquipo.objects.filter(rol = rolSM, proyecto_id = id_proyecto)
+    if haySM.count() > 0: #si hay Scrum Master
+        SM = MiembroEquipo.objects.get(rol = rolSM, proyecto_id = id_proyecto)
+        SMUser = User.objects.get(id = SM.usuario_id)
+    else:
+        SMUser = None
     dato = get_object_or_404(Proyecto, pk=id_proyecto)
     equipos = MiembroEquipo.objects.filter(proyecto_id = id_proyecto)
-    #usuarios = User.objects.filter(id = equipos.usuario)
-
-    # equipi = MiembroEquipo.objects.get(proyecto_id = id_proyecto)
-    # comite = User.objects.filter(miembroequipo__id=id_proyecto) #filtra usuarios del comite
-    #lider = get_object_or_404(User, pk=dato.lider_id)
-    #flujos = Flujo.objects.filter(proyecto_id=id_proyecto)
-    # nombre_roles = []
-    usuarios = []
-    #
-    # roles = Group.objects.filter(proyecto_id=id_proyecto)
-    # for rol in roles:
-    #     nombre_roles.append(rol)
-    #     u = User.objects.filter(groups__id=rol.id)
-    #
-    for nose in equipos:
-        u = User.objects.filter(id = nose.usuario_id)
-        #rol = Group.objects.filter(id = nose.rol_id)
-        for user in u:
-            uu = user.first_name + " " + user.last_name  + ", "  +"\n"
-            usuarios.append(uu)
 
 
     return render_to_response('equipos/ver_equipo.html',
-                          {'proyectos': dato, 'usuarios': usuarios, 'scrumMaster': SMUser},
+                          {'proyecto': dato, 'equipos': equipos, 'scrumMaster': SMUser},
                           context_instance=RequestContext(request))
 
 
